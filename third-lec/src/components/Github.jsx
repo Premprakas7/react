dae49e612d2d5060d05d90659e6f4e5318924214
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2022-05-23 10:03:00 +0530

    sec

--- third-lec/src/components/Github.jsx
+++ third-lec/src/components/Github.jsx
@@ -2,26 +2,32 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 import "./Github.css"
 
-const GithubUserRepo=(q = "react" )=>{
+const GithubUserRepo=(q = "react" ,page=1 )=>{
  return (axios("https://api.github.com/search/repositories",{
- method:"GET",  
- params:{q:q}})
+ method:"GET",
+   
+ params:{q:q,
+  per_page:5,
+ page,
+}})
 
 )}
 
 export const Github = () => {
     const [loading,setLoading]=useState(true);
     const [error,setError]=useState(false);
-    const [query,setQuery]=useState(true);
+    const [query,setQuery]=useState("react");
     const [data, setData]=useState([]);
+    const [page,setPage]=useState(1);
 
-useEffect(()=>{GithubUserRepo(query)
+useEffect(()=>{GithubUserRepo(query,page)
 .then((res)=>
 { setLoading(false)
   setData(res.data)})
 .catch((err)=>{setError(true)})
-})
+},[query,page])
 const handleClick=(query)=>setQuery(query)
+//const PageChange=page=>setPage(page)
 console.log(data)
 
 
@@ -31,6 +37,10 @@ console.log(data)
       {loading && <div>...loading</div>}
       <SearchBox handleClick={handleClick}/>
       {data?.items?.map((item)=><GithubCard key={item.id} {...item}/>)}
+      <div>
+        <button disabled={page===1} onClick={()=>setPage(page-1)}>PREV</button>
+        <button onClick={()=>setPage(page+1)}>NEXT</button>
+      </div>
     </div>
   )
 }
@@ -40,7 +50,7 @@ const SearchBox=({handleClick})=>{
   return(
     <div>
       <input type="text" onChange={e=>setText(e.target.value)} />
-    <button onClick={handleClick}>SEARCH</button>
+    <button onClick={()=>{handleClick(text)}}>SEARCH</button>
     </div>
     
   )
