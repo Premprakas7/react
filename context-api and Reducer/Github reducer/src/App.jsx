2e1822c7f145421b997a1a495da96bd1932037b8
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2022-05-26 23:50:42 +0530

    commit

--- context-api and Reducer/Github reducer/src/App.jsx
+++ context-api and Reducer/Github reducer/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { AppContextProvider } from './AppContext/AppContextProvider'
+import Github from './Components/Github'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,8 @@ function App() {
   return (
     <div className="App">
      
+     <h2>Prem</h2>
+     <Github/>
     </div>
   )
 }
