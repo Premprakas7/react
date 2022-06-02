9e149b9d3d526222f2d5727ba732d1803b8940d1
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2022-06-02 11:09:16 +0530

    i

--- Routing/tannerGoods/src/Components/AllRoute.jsx
+++ Routing/tannerGoods/src/Components/AllRoute.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import {Routes,Route,Link} from "react-router-dom"
 import Home from './Home'
+import "./home.css"
 
 const Navbar=()=>{
     return(
-        <nav>
-            <Link to='/' ><img src="" alt="logo" /></Link>
-            <Link to=''></Link>
+        <nav className='nav'>
+            <div>
+            <Link to='/' ><img className='pumalogo' src="https://assets.turbologo.com/blog/en/2019/11/19084917/puma-logo-cover-958x575.png" alt="logo" /></Link>
+            </div>
+            <div>
+                <Link to='/newarrival'>New Arrival</Link>
+            </div>
+            <Link to='/login'>login</Link>
         </nav>
     )
 }
